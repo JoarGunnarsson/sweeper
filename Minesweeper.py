1de68472fb61b52976f10809625d662fce9040c4
Joar Gunnarsson <joar.gunnarsson@gmail.com>
2018-04-08 15:33:08 +0200

    Started adding different sizes

--- Minesweeper.py
+++ Minesweeper.py
@@ -3,15 +3,16 @@
 #Square A2 (index '1') showed the number '0', while it should have been a '1'. Might be because of 'grid[x-1][3]' thingy.
 #Square B2 (index '10') showed the number '1', while it should have been a '2'. Might be because of ^.
 #Do so that even numbers in the adjacent can be '?'.
+#Add more map-sizes
 import os
 import random
 import time
 import save
 yaxis = ["a","b","c","d","e","f","g","h","i"]
 xaxis = [0,1,2,3,4,5,6,7,8,9,"1","2","3","4","5","6","7","8","9"]
-a = 1
-b = 2
-c = 3
+a = 0
+b = 0
+c = 0
 health = 1
 while True:
     play = "yes"
@@ -27,12 +28,23 @@ while True:
             if contents != "":
                 grid = save.savegrid
                 health = save.health
+                size = save.size
                 print ("Board has been loaded.")
                 times = 1
                 break
             else:
                 print ("The save file is empty, cannot load save!")
         else:
+            while True:
+                size = input("Select size of the grid: '9x9', '20x20'.\n")
+                if "9" in size:
+                    size = 9
+                    break
+                elif "20" in size:
+                    size = 9
+                    break
+                else:
+                    print ("That is not a valid size!")
             while True:
                 health = input("What difficulty do you want to play with? 'Hard', 'Medium' or 'Easy'?\n")
                 health = health.lower()
@@ -48,44 +60,49 @@ while True:
                 else:
                     print ("That's not a valid difficulty!\n")
             break
-    print ("Enter /save at any time to save the game.\n")
+    print ("\nEnter /save at any time to save the game.\n")
     if "load" not in action:
         print ("Starting Minesweeper...")
         print ("? on a square means it has not been sweeped yet, ! means a squared has been flagged, and the numbers tell you how many mines are adjacent to that square.")
         print ("When you have flagged all the mines, you have to sweep all the remaining squares to complete the game.\n")
+        time.sleep(c+a)
         print ("Generating board...")
-        time.sleep(c+b)
+        time.sleep(a)
         c, r = 6, 81
         grid = [[0 for x in range(c)] for y in range(r)]
-        for x in range(len(grid)):
-            letter = x/9
-            if letter < 1:
-                grid[x][0] = "a"
-                grid[x][1] = x+1
-            if letter >= 1 and letter < 2:
-                grid[x][0] = "b"
-                grid[x][1] = x-8
-            if letter >= 2 and letter < 3:
-                grid[x][0] = "c"
-                grid[x][1] = x-16-1
-            if letter >= 3 and letter < 4:
-                grid[x][0] = "d"
-                grid[x][1] = x-24-2
-            if letter >= 4 and letter < 5:
-                grid[x][0] = "e"
-                grid[x][1] = x-32-3
-            if letter >= 5 and letter < 6:
-                grid[x][0] = "f"
-                grid[x][1] = x-40-4
-            if letter >= 6 and letter < 7:
-                grid[x][0] = "g"
-                grid[x][1] = x-48-5
-            if letter >= 7 and letter < 8:
-                grid[x][0] = "h"
-                grid[x][1] = x-56-6
-            if letter >= 8 and letter < 9:
-                grid[x][0] = "i"
-                grid[x][1] = x-64-7
+        if size == 9:
+            for x in range(len(grid)):
+                letter = x/9
+                if letter < 1:
+                    grid[x][0] = "a"
+                    grid[x][1] = x+1
+                if letter >= 1 and letter < 2:
+                    grid[x][0] = "b"
+                    grid[x][1] = x-8
+                if letter >= 2 and letter < 3:
+                    grid[x][0] = "c"
+                    grid[x][1] = x-16-1
+                if letter >= 3 and letter < 4:
+                    grid[x][0] = "d"
+                    grid[x][1] = x-24-2
+                if letter >= 4 and letter < 5:
+                    grid[x][0] = "e"
+                    grid[x][1] = x-32-3
+                if letter >= 5 and letter < 6:
+                    grid[x][0] = "f"
+                    grid[x][1] = x-40-4
+                if letter >= 6 and letter < 7:
+                    grid[x][0] = "g"
+                    grid[x][1] = x-48-5
+                if letter >= 7 and letter < 8:
+                    grid[x][0] = "h"
+                    grid[x][1] = x-56-6
+                if letter >= 8 and letter < 9:
+                    grid[x][0] = "i"
+                    grid[x][1] = x-64-7
+        elif size == 20:
+            #Generate Board!
+            pass
         for x in range(len(grid)):
             grid[x][2] = "?"
             grid[x][3] = "N/A"
@@ -94,27 +111,35 @@ while True:
     time.sleep(b)
     os.system('cls')
     while play != "no" and health != 0:
-        print ("     [Health: %d]"%(health))
-        print ("      1  2  3  4  5  6  7  8  9 ")
-        print ("A    [{}][{}][{}][{}][{}][{}][{}][{}][{}]".format(grid[0][2], grid[1][2],grid[2][2],grid[3][2],grid[4][2],grid[5][2],grid[6][2],grid[7][2],grid[8][2]))
-        print ("B    [{}][{}][{}][{}][{}][{}][{}][{}][{}]".format(grid[9][2], grid[10][2],grid[11][2],grid[12][2],grid[13][2],grid[14][2],grid[15][2],grid[16][2],grid[17][2]))
-        print ("C    [{}][{}][{}][{}][{}][{}][{}][{}][{}]".format(grid[18][2], grid[19][2],grid[20][2],grid[21][2],grid[22][2],grid[23][2],grid[24][2],grid[25][2],grid[26][2]))
-        print ("D    [{}][{}][{}][{}][{}][{}][{}][{}][{}]".format(grid[27][2], grid[28][2],grid[29][2],grid[30][2],grid[31][2],grid[32][2],grid[33][2],grid[34][2],grid[35][2]))
-        print ("E    [{}][{}][{}][{}][{}][{}][{}][{}][{}]".format(grid[36][2], grid[37][2],grid[38][2],grid[39][2],grid[40][2],grid[41][2],grid[42][2],grid[43][2],grid[44][2]))
-        print ("F    [{}][{}][{}][{}][{}][{}][{}][{}][{}]".format(grid[45][2], grid[46][2],grid[47][2],grid[48][2],grid[49][2],grid[50][2],grid[51][2],grid[52][2],grid[53][2]))
-        print ("G    [{}][{}][{}][{}][{}][{}][{}][{}][{}]".format(grid[54][2], grid[55][2],grid[56][2],grid[57][2],grid[58][2],grid[59][2],grid[60][2],grid[61][2],grid[62][2]))
-        print ("H    [{}][{}][{}][{}][{}][{}][{}][{}][{}]".format(grid[63][2], grid[64][2],grid[65][2],grid[66][2],grid[67][2],grid[68][2],grid[69][2],grid[70][2],grid[71][2]))
-        print ("I    [{}][{}][{}][{}][{}][{}][{}][{}][{}]".format(grid[72][2], grid[73][2],grid[74][2],grid[75][2],grid[76][2],grid[77][2],grid[78][2],grid[79][2],grid[80][2]))
+        if size == 9:
+            if times == 0:
+                print ("     [Mines: ?] [Flags placed: 0]")
+            print ("      1  2  3  4  5  6  7  8  9 ")
+            print ("A    [{}][{}][{}][{}][{}][{}][{}][{}][{}]".format(grid[0][2], grid[1][2],grid[2][2],grid[3][2],grid[4][2],grid[5][2],grid[6][2],grid[7][2],grid[8][2]))
+            print ("B    [{}][{}][{}][{}][{}][{}][{}][{}][{}]".format(grid[9][2], grid[10][2],grid[11][2],grid[12][2],grid[13][2],grid[14][2],grid[15][2],grid[16][2],grid[17][2]))
+            print ("C    [{}][{}][{}][{}][{}][{}][{}][{}][{}]".format(grid[18][2], grid[19][2],grid[20][2],grid[21][2],grid[22][2],grid[23][2],grid[24][2],grid[25][2],grid[26][2]))
+            print ("D    [{}][{}][{}][{}][{}][{}][{}][{}][{}]".format(grid[27][2], grid[28][2],grid[29][2],grid[30][2],grid[31][2],grid[32][2],grid[33][2],grid[34][2],grid[35][2]))
+            print ("E    [{}][{}][{}][{}][{}][{}][{}][{}][{}]".format(grid[36][2], grid[37][2],grid[38][2],grid[39][2],grid[40][2],grid[41][2],grid[42][2],grid[43][2],grid[44][2]))
+            print ("F    [{}][{}][{}][{}][{}][{}][{}][{}][{}]".format(grid[45][2], grid[46][2],grid[47][2],grid[48][2],grid[49][2],grid[50][2],grid[51][2],grid[52][2],grid[53][2]))
+            print ("G    [{}][{}][{}][{}][{}][{}][{}][{}][{}]".format(grid[54][2], grid[55][2],grid[56][2],grid[57][2],grid[58][2],grid[59][2],grid[60][2],grid[61][2],grid[62][2]))
+            print ("H    [{}][{}][{}][{}][{}][{}][{}][{}][{}]".format(grid[63][2], grid[64][2],grid[65][2],grid[66][2],grid[67][2],grid[68][2],grid[69][2],grid[70][2],grid[71][2]))
+            print ("I    [{}][{}][{}][{}][{}][{}][{}][{}][{}]".format(grid[72][2], grid[73][2],grid[74][2],grid[75][2],grid[76][2],grid[77][2],grid[78][2],grid[79][2],grid[80][2]))
+            print ("[Health: %d]"%(health))
+        elif size == 20:
+            #Do something.
+            pass
         while True:
             if times == 0:
                 square = input("Enter the coordinates of the square you want to sweep/flag (e.g A5) and what you want to do with it ('flag', 'unflag' or 'sweep').\n")
             else:
-                square = input("Enter the coordinates of the square.\n")
+                square = input("Enter the coordinates.\n")
             square = square.lower()
             if "save" in square:
                 print ("Alright, saving the board.")
                 f = open("save.py","w")
-                f.write("health = ")
+                f.write("size = ")
+                f.write(str(size))
+                f.write("\nhealth = ")
                 f.write(str(health))
                 f.write("\nsavegrid = ")
                 f.write(str(grid))
