d5865b76fae88202c95ef028061fe354e03e42de
Joar Gunnarsson <joar.gunnarsson@gmail.com>
2018-04-09 18:00:51 +0200

    Simplified adj list creation

--- MainFile.py
+++ MainFile.py
@@ -27,7 +27,7 @@ while True:
                 print ("Board has been loaded.")
                 break
             else:
-                print ("The save file is empty, cannot load save!")
+                print ("The save file is empty, save cannot be loaded!")
         else:
             while True:
                 boardSize = input("Select size of the grid: '9x9', '20x20'.\n")
@@ -90,16 +90,14 @@ while True:
     time.sleep(b)
     os.system('cls')
     while play != "no" and health != 0:
+        if times == 0:
+            print ("     [Mines: ?] [Flags placed: 0]")
         if boardSize == 9:
-            if times == 0:
-                print ("     [Mines: ?] [Flags placed: 0]")
-            if boardSize == 9:
-                print ("      1  2  3  4  5  6  7  8  9 ")
-            elif boardSize == 20:
-                print ("      1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20")
+            print ("      1  2  3  4  5  6  7  8  9 ")
             for x in range(0,9):
                 print ("{}    [{}][{}][{}][{}][{}][{}][{}][{}][{}]".format(yaxis[x].upper(),grid[9*x][2],grid[9*x+1][2],grid[9*x+2][2],grid[9*x+3][2],grid[9*x+4][2],grid[9*x+5][2],grid[9*x+6][2],grid[9*x+7][2],grid[9*x+8][2]))
         elif boardSize == 20:
+            print ("      1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20")
             for x in range(0,20):
                 print ("{}    [{}][{}][{}][{}][{}][{}][{}][{}][{}][{}][{}][{}][{}][{}][{}][{}][{}][{}][{}][{}]".format(yaxis[x].upper(),grid[20*x][2],grid[20*x+1][2],grid[20*x+2][2],grid[20*x+3][2],grid[20*x+4][2],grid[20*x+5][2],grid[20*x+6][2],grid[20*x+7][2],grid[20*x+8][2],grid[20*x+9][2],grid[20*x+10][2],grid[20*x+11][2],grid[20*x+12][2],grid[20*x+13][2],grid[20*x+14][2],grid[20*x+15][2],grid[20*x+16][2],grid[20*x+17][2],grid[20*x+18][2],grid[20*x+19][2]))
         print ("[Health: %d]"%(health))
@@ -111,15 +109,9 @@ while True:
             square = square.lower()
             if "save" in square:
                 print ("Alright, saving the board.")
+                string = "boardSize = "+str(boardSize)+"\nhealth = "+str(health)+"\ntimes = "+str(times)+"\nsavegrid = "+str(grid)
                 f = open("save.py","w")
-                f.write("boardSize = ")
-                f.write(str(boardSize))
-                f.write("\nhealth = ")
-                f.write(str(health))
-                f.write("\ntimes = ")
-                f.write(str(times))
-                f.write("\nsavegrid = ")
-                f.write(str(grid))
+                f.write(string)
                 f.close()
                 print ("Done.\n")
                 time.sleep(b)
@@ -131,41 +123,80 @@ while True:
                 square = square.replace("unflag","")
                 square = square.replace(" ","")
                 square = list(square)
-                if len(square) > 2: ###This does not accept t20 sweep! Only two numbers, not good!
-                    print ("Write the coordinates of the square properly.")
-                if square[0] not in yaxis:
-                    print ("Write the coordinates of the square properly.")
-                elif square[1] not in xaxis:
-                    print ("Write the coordinates of the square properly.")
+                if square[0] in yaxis:######
+                    square0 = square[0]#Saves the letter of the square
+                    square.pop(0)#Removes the letter from square
+                    square = [int("".join(square))]#Joins the numbers of the square together.
+                    square.insert(0,square0)#Adds the letter back.
+                    if square[0] in yaxis and square[1] in xaxis:
+                        break
+                    else:
+                        print ("Write the coordinates of the square properly.")
+                elif square[1] in yaxis:
+                    if len(square) > 2:
+                        print ("Write the coordinates of the square properly.")
+                    else:
+                        square1 = square[1]#Saves the letter of the square
+                        square.pop(1)#Removes the letter from square
+                        square = [int("".join(square))]#Joins the numbers of the square together.
+                        square.insert(0,square1)#Adds the letter back.
+                        if square[0] in yaxis and square[1] in xaxis:
+                            break
+                        else:
+                            print ("Write the coordinates of the square properly.")
+                elif square[2] in yaxis:
+                    square2 = square[2]#Saves the letter of the square
+                    square.pop(2)#Removes the letter from square
+                    square = [int("".join(square))]#Joins the numbers of the square together.
+                    square.insert(0,square2)#Adds the letter back.
+                    if square[0] in yaxis and square[1] in xaxis:
+                        break
+                    else:
+                        print ("Write the coordinates of the square properly.")
                 else:
-                    if square [1] in xaxis:
-                        square[1] = int(square[1])
-                    elif square [1] in yaxis:
-                        square[1] = int(square[0])
-                    break
+                    print ("Write the coordinates of the square properly.")
             elif 'flag' in square:
                 action = 'flag'
                 square = square.replace("flag","")
                 square = square.replace(" ","")
                 square = list(square)
-                if len(square) != 2: ###This does not accept t20 sweep! Only two numbers, not good!
-                    print ("Write the coordinates of the square properly.")
-                elif square[0] not in yaxis:
-                    print ("Write the coordinates of the square properly.")
-                elif square[1] not in xaxis:
-                    print ("Write the coordinates of the square properly.")
+                if square[0] in yaxis:######
+                    square0 = square[0]#Saves the letter of the square
+                    square.pop(0)#Removes the letter from square
+                    square = [int("".join(square))]#Joins the numbers of the square together.
+                    square.insert(0,square0)#Adds the letter back.
+                    if square[0] in yaxis and square[1] in xaxis:
+                        break
+                    else:
+                        print ("Write the coordinates of the square properly.")
+                elif square[1] in yaxis:
+                    if len(square) > 2:
+                        print ("Write the coordinates of the square properly.")
+                    else:
+                        square1 = square[1]#Saves the letter of the square
+                        square.pop(1)#Removes the letter from square
+                        square = [int("".join(square))]#Joins the numbers of the square together.
+                        square.insert(0,square1)#Adds the letter back.
+                        if square[0] in yaxis and square[1] in xaxis:
+                            break
+                        else:
+                            print ("Write the coordinates of the square properly.")
+                elif square[2] in yaxis:
+                    square2 = square[2]#Saves the letter of the square
+                    square.pop(2)#Removes the letter from square
+                    square = [int("".join(square))]#Joins the numbers of the square together.
+                    square.insert(0,square2)#Adds the letter back.
+                    if square[0] in yaxis and square[1] in xaxis:
+                        break
+                    else:
+                        print ("Write the coordinates of the square properly.")
                 else:
-                    if square [1] in xaxis:
-                        square[1] = int(square[1])
-                    elif square [1] in yaxis:
-                        square[1] = int(square[0])
-                    break
+                    print ("Write the coordinates of the square properly.")
             elif 'sweep' in square:
                 action = 'sweep'
                 square = square.replace("sweep","")
                 square = square.replace(" ","")
                 square = list(square)
-                print (square)
                 if square[0] in yaxis:######
                     square0 = square[0]#Saves the letter of the square
                     square.pop(0)#Removes the letter from square
@@ -201,7 +232,6 @@ while True:
             else:
                 print ("You have to include the action!")
         for x in range (len(grid)):
-            print (grid[x])
             if grid[x][0] == square[0] and grid[x][1] == square[1]:
                 if grid[x][4] == "yes":
                     if action == "sweep":
@@ -221,48 +251,30 @@ while True:
                         grid[x][3] = "number"
                         grid[x][4] = "yes"
                         y = x
-                        if boardSize == 9:
-                            if grid[x][1] == 1:#This depends on boardSize#### For other boardSizes, adj isn not +10 etc.
-                                if grid[x][0] == "a":
-                                    adj = [grid[y+1], grid[y+10], grid[y+9]]
-                                elif grid[x][0] == "i":
-                                    adj = [grid[y+1], grid[y-9], grid[y-8]]
-                                else:
-                                    adj = [grid[y+1], grid[y-9], grid[y-8], grid[y+10], grid[y+9]]
-                            elif grid[x][1] == 9:
-                                if grid[x][0] == "a":
-                                    adj = [grid[y-1], grid[y+9], grid[y+8]]
-                                elif grid[x][0] == "i":
-                                    adj = [grid[y-1], grid[y-10], grid[y-9]]
-                                else:
-                                    adj = [grid[y-1], grid[y-10], grid[y-9], grid[y+9], grid[y+8]]
-                            elif grid[x][0] == "a":
-                                adj = [grid[y-1], grid[y+1], grid[y+10], grid[y+9], grid[y+8]]
-                            elif grid[x][0] == "i":
-                                adj = [grid[y-1], grid[y+1], grid[y-10], grid[y-9], grid[y-8]]
-                            else:
-                                adj = [grid[y-1], grid[y+1], grid[y-10], grid[y-9], grid[y-8], grid[y+10], grid[y+9], grid[y+8]]#FULLSQUARE
+                        toRemove = []
+                        if boardSize == 9:#Use the below thing on the 60 lines long adj code below.
+                            adj = [grid[y-1], grid[y+1], grid[y-10], grid[y-9], grid[y-8], grid[y+10], grid[y+9], grid[y+8]]#Starta med fullsquare, pop():a de som inte är rätt.
                         elif boardSize == 20:
-                            if grid[x][1] == 1:
-                                if grid[x][0] == "a":
-                                    adj = [grid[y+1], grid[y+20], grid[y+21]]
-                                elif grid[x][0] == "t":
-                                    adj = [grid[y+1], grid[y-19], grid[y-20]]
-                                else:
-                                    adj = [grid[y+1], grid[y+20], grid[y+21], grid[y-19], grid[y-20]]
-                            elif grid[x][1] == 20:
-                                if grid[x][0] == "a":
-                                    adj = [grid[y-1], grid[y+19], grid[y+20]]
-                                elif grid[x][0] == "t":
-                                    adj = [grid[y-1], grid[y-20], grid[y-21]]
-                                else:
-                                    adj = [grid[y-1], grid[y+19], grid[y+20], grid[y-20], grid[y-21]]
-                            elif grid[x][0] == "a":###
-                                adj = [grid[y-1], grid[y+1], grid[y+19], grid[y+20], grid[y+21]]
-                            elif grid[x][0] == "t":
-                                adj = [grid[y-1], grid[y+1], grid[y-19], grid[y-20], grid[y-21]]
-                            else:
-                                adj = [grid[y-1], grid[y+1], grid[y-19], grid[y-20], grid[y-21], grid[y+19], grid[y+20], grid[y+21]]#FULLSQUARE
+                            adj = [grid[y-1], grid[y+1], grid[y-21], grid[y-20], grid[y-19], grid[y+21], grid[y+20], grid[y+19]]#Simplify adjacent, use adj.append(grid[tja... vad ska man säga?])
+                        if grid[y][1] == 1:
+                            toRemove.append(adj[0])
+                            toRemove.append(adj[2])
+                            toRemove.append(adj[7])
+                        if grid[y][1] == 9:
+                            toRemove.append(adj[1])
+                            toRemove.append(adj[4])
+                            toRemove.append(adj[5])
+                        if grid[y][0] == "a":
+                            toRemove.append(adj[5])
+                            toRemove.append(adj[6])
+                            toRemove.append(adj[7])
+                        if grid[y][0] == "i":
+                            toRemove.append(adj[2])
+                            toRemove.append(adj[3])
+                            toRemove.append(adj[4])
+                        for x in range(len(adj)):
+                            if toRemove[x] in adj:
+                                adj.remove(toRemove[x])
                         break
                     else:
                         if grid[x][2] == "!":
@@ -298,7 +310,7 @@ while True:
                     else:
                         grid[x][3] = "number"
         if times == 0:
-            for x in range (len(grid)):#The code below looks like it needs simplifying.
+            for x in range (len(grid)):#The code below looks like it needs simplifying. It also doesn't work for 20x20. Change to for x in range(len(adj)): if adj[x][3] == "x"
                 if grid[x][3] != "x" and grid[x][3] != "N/A":
                     number = 0
                     if grid[x][1] != 1 and grid[x][1] != 9:
@@ -393,15 +405,17 @@ while True:
         if times > 0:
             if health != 0:
                 print ("     [Mines: {}] [Flags placed: {}]".format(mines, flags))
-    action = input("Do you want to go again? Y/N \n")
-    action = action.lower()
-    if "n" in action:
-        print ("Alright, exiting.")
-        timme.sleep(c)
-        import sys
-        sys.exit()
-    elif "y" in action:
-        print ("Alright, restarting...")
-        time.sleep(c)
-    else:
-        print ("%d is not an option." %(action))
+    while True:
+        action = input("Do you want to go again? Y/N \n")
+        action = action.lower()
+        if "n" in action:
+            print ("Alright, exiting.")
+            timme.sleep(c)
+            import sys
+            sys.exit()
+        elif "y" in action:
+            print ("Alright, restarting...")
+            time.sleep(c)
+            break
+        else:
+            print ("%d is not an option." %(action))
